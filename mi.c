61f6a20b5c466524bcd87f9e0f121a006de93243
jinseo <jinkwon980702@gmail.com>
2025-02-15 14:44:24 +0900

    push

--- mi.c
+++ mi.c
@@ -1,6 +1,7 @@
 #include "./mi.h"
 
 char **te;
+int to;
 
 int slen(char *str)
 {
@@ -24,26 +25,14 @@ int check_op(char *op)
 int cnt_op(char **argv)
 {
 	int cnt = 0;
-	int idx = 1;
-	while (check_op(argv[idx]))
-		idx++;
+	int idx = 0;
+	if (check_op(argv[++idx]) == 0)
+		++cnt;
 	while (argv[idx])
 	{
-		while (check_op(argv[idx]))
-		{
-			if (check_op(argv[idx]) == -1)
-				break;
-			if (!check_op(argv[idx + 1]))
-			{
-				if (check_op(argv[idx + 1]) == -1)
-					break;
-				cnt++;
-				break;
-			}
-			idx++;
-		}
-		if (check_op(argv[idx]) == -1)
-			break;
+		int c_f = check_op(argv[idx]);
+		if (c_f > 0 && check_op(argv[idx + 1]) == 0)
+			++cnt;
 		idx++;
 	}
 	return (cnt);
@@ -52,37 +41,28 @@ int cnt_op(char **argv)
 int op_addr(char **argv, int st)
 {
 	int cnt = 0;
-	int idx = 1;
-	while (check_op(argv[idx]))
-		idx++;
+	int idx = 0;
+	if (check_op(argv[++idx]) == 0)
+		++cnt;
 	while (argv[idx])
 	{
-		while (check_op(argv[idx]))
+		int c_f = check_op(argv[idx]);
+		if (c_f > 0 && check_op(argv[idx + 1]) == 0)
 		{
-			if (check_op(argv[idx]) == -1)
-			break;
-			if (!check_op(argv[idx + 1]))
-			{
-				if (check_op(argv[idx + 1]) == -1)
-				break;
-				cnt++;
-				break;
-			}
+			++cnt;
 			idx++;
 		}
-		if (check_op(argv[idx]) == -1)
-		break;
-		idx++;
-		if (cnt == st)
+		if (cnt - 1 == st)
 			return (idx);
+		idx++;
 	}
-	return (0);
+	return (cnt);
 }
 
 void start(char **argv, int cnt)
 {
 	int st = 0;
-	while (st <= cnt)
+	while (st < cnt)
 	{
 		start_fork(argv, cnt, st);
 		st++;
@@ -94,17 +74,92 @@ void start_fork(char **argv, int cnt, int st)
 {
 	int op_flag = 0;
 	int idx = op_addr(argv, st);
+	int idx2 = op_addr(argv, st);
 	while (!check_op(argv[idx]))
 		idx++;
 	op_flag = check_op(argv[idx]);
-	printf("%d cnt %d st %d Hello\n", op_flag, cnt, st);//check
-	
+	printf("idx2 is %d idx is %d last op %d cnt %d st %d Hello\n", idx2 ,idx ,op_flag, cnt, st);//check
+	if (op_flag != 2)
+	{
+		fork_semi(argv, idx2, idx, st);
+		return ;
+	}
+	int pid = -1;
+	int fd[2];
+	pid = fork();//error 처리
+	pipe(fd);//error처리
+	if (pid < 0)
+	{
+		//error처리
+	}
+	else if (pid == 0)
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		close(to);
+		int e_f = 0;
+		argv[idx] = NULL;
+		e_f = execve(argv[idx2], &argv[idx2], te);
+		if (e_f < 0)
+		{
+			printf("error\n");
+			exit(1);
+		}
+	}
+	else
+	{
+		dup2(fd[0], 0);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pid, NULL, 0);
+	}	
 }
+
+void fork_semi(char **argv, int first_cmd, int null_space, int st)
+{
+	int pid = -1;
+	pid = fork();
+	if (pid < 0)
+	{
+		printf("error\n");
+	}
+	else if (pid == 0)
+	{
+		int e_f = 0;
+		if (st > 0)
+		{
+			printf("%s\n", argv[first_cmd - 1]);
+			if(check_op(argv[first_cmd - 1]) == 3)
+			{
+				dup2(0, to);//error처리하기
+			}
+		}
+		close(to);
+		argv[null_space] = NULL;
+		e_f = execve(argv[first_cmd], &argv[first_cmd], te);
+		if (e_f < 0)
+		{
+			printf("error\n");
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+}
+
+void fork_pipe()
+{}
 
 int main(int argc, char *argv[], char *envp[])
 {
 	te = envp;
+	to = dup(0);
 	int cnt = cnt_op(argv);
 	start(argv, cnt);
+
+	close(to);
 	return (0);
 }
--- mi.h
+++ mi.h
@@ -2,6 +2,7 @@
 #define M_H
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
@@ -13,4 +14,5 @@ int cnt_op(char **argv);
 int op_addr(char **argv, int st);
 void start(char **argv, int cnt);
 void start_fork(char **argv, int cnt, int st);
+void fork_semi(char **argv, int first_cmd, int null_space, int st);
 #endif
